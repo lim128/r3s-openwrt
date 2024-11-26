f4943740bca21306243585f615f6486f5137cac3
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-11-26 17:16:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,8 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
