353092904486f33ef940aed4a3fc8bf7a43d772b
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-11-30 03:15:03 +0800

    Create diy-part1-1.sh

---
+++ diy-part1-1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+wget -O target/linux/ramips/patches-4.14/102-mt7621-fix-cpu-clk-add-clkdev.patch https://ghproxy.net/https://raw.githubusercontent.com/qqww4540/rule/main/102-mt7621-fix-cpu-clk-add-clkdev.patch
