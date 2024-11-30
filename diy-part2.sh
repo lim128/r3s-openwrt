5c97e9942fb4c8fb411c1e902cecb6149e55900b
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-11-30 15:15:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,7 @@ sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_genera
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
