34aa47e8dc99ef232e1e6d29022b0c7cf829ed11
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-12-06 07:45:02 +0800

    Update diy-part1-1.sh

--- diy-part1-1.sh
+++ diy-part1-1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 wget -O target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch https://raw.githubusercontent.com/qqww4540/rule/refs/heads/main/322-mt7621-fix-cpu-clk-add-clkdev.patch
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
