88825a234a389d5a2a363a34a60d217f751d7e1c
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-12-29 21:36:22 +0800

    Update diy-part2-1.sh

--- diy-part2-1.sh
+++ diy-part2-1.sh
@@ -22,3 +22,4 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 sed -i 's/LEDE/NanoPi/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/openwrt-stuntman.git package/stuntman
