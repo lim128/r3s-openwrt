14bcdb9086f061ed4db5a291a24b3b56cfc41840
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-12-30 00:55:01 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone -b master --single-branch https://github.com/muink/openwrt-stuntman.git package/stuntman
+
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/NanoPi/g' package/base-files/files/bin/config_generate
