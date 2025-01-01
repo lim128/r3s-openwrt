6aaf10ca629ca86d1165089c8302bc45cc8a0e20
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2025-01-01 10:25:57 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -18,3 +18,6 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
