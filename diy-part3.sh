ceeb77914a692b06d72c0a6a5009ce8871e35305
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2025-01-02 12:37:23 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -14,8 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone -b master --single-branch https://github.com/kenzok8/openwrt-packages.git package/kenzo
+git clone -b master --single-branch https://github.com/kenzok8/small.git package/small
+git clone -b main https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
 git clone -b master --single-branch https://github.com/muink/openwrt-stuntman.git package/stuntman
